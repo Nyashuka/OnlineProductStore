6f3c73097c1fe385d953829a34763b75389c205a
Nyashuka <godnebeles@gmail.com>
2023-11-15 16:38:06 +0200

    create project

---
+++ OnlineProductStore/Shared/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineProductStore.Shared.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [DataType(DataType.Currency)]
+        public required string PriceWithDiscount { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string? Image { get; set; } 
+
+        public DateTime UploadedTime { get; set; } = DateTime.Now;
+    }
+}
